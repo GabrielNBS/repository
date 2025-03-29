226442d9ef6d241c9e0b429b052a11c3b86e8e88
Gabriel Nascimento <gabrielganbajf@gmail.com>
2025-03-28 21:52:51 -0300

    style: refactor section 'About' to include icon technologies and improve responsiveness using carvaways

--- src/Containers/About/About.tsx
+++ src/Containers/About/About.tsx
@@ -4,17 +4,56 @@ import cards from '../../cards/cards'
 import CardSkills from '../../Components/SkillCard/SkillCard'
 import { Text } from '../../Components/Text/styles'
 
+import {
+  SiNextdotjs,
+  SiRedux,
+  SiTypescript,
+  SiStyledcomponents,
+  SiTailwindcss,
+  SiSass,
+  SiPostman,
+  SiGit,
+} from 'react-icons/si'
+import { FaReact, FaNodeJs } from 'react-icons/fa'
+
+const techs = [
+  { icon: <SiNextdotjs size={20} />, name: 'Next.js' },
+  { icon: <SiRedux size={20} />, name: 'Redux' },
+  { icon: <SiTypescript size={20} />, name: 'TypeScript' },
+  { icon: <FaReact size={20} />, name: 'React.js' },
+  { icon: <SiStyledcomponents size={20} />, name: 'Styled-Components' },
+  { icon: <SiTailwindcss size={20} />, name: 'Tailwind' },
+  { icon: <SiSass size={20} />, name: 'Sass' },
+  { icon: <SiPostman size={20} />, name: 'Rest' },
+  { icon: <FaNodeJs size={20} />, name: 'Node.js' },
+  { icon: <SiGit size={20} />, name: 'Git' },
+]
+
 export default function About() {
   return (
     <S.AboutSection>
-      <div className="card_description">
-        <Text as="h2">Como eu te ajudo</Text>
-        <Text>
-          <strong>✨ Tudo começa com uma necessidade</strong>
-          Eu a transformo em interfaces que conectam, inspiram e entregam
-          resultados. Combinando tecnologia e design, crio experiências digitais
-          que fazem a diferença.
-        </Text>
+      <div className="about_container">
+        <div className="description_container">
+          <Text as="h2">Como eu te ajudo</Text>
+          <Text>
+            <strong>✨ Tudo começa com uma necessidade</strong>
+            Eu a transformo em interfaces que conectam, inspiram e entregam
+            resultados. Combinando tecnologia e design, crio experiências
+            digitais que fazem a diferença utilizando tecnologias modernas como:
+          </Text>
+        </div>
+        <div className="tech_container">
+          <div className="tech-carousel">
+            <ul>
+              {techs.map((tech, index) => (
+                <li key={index}>
+                  {tech.icon}
+                  {tech.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
       <div className="card_container">
         {cards.map((card, index) => (
--- src/Containers/About/styles.ts
+++ src/Containers/About/styles.ts
@@ -1,5 +1,15 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { CardBox } from '../../Components/SkillCard/styles'
+import { centralize } from '../../keyframes/Centralize'
+
+const scrollAnimation = keyframes`
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-50%);
+  }
+`
 
 export const AboutSection = styled.section`
   position: relative;
@@ -8,11 +18,12 @@ export const AboutSection = styled.section`
   grid-template-columns: 1fr 1fr;
   row-gap: 1rem;
 
-  .card_description {
+  .about_container {
     display: flex;
-    align-self: center;
     flex-direction: column;
+    padding-left: 3rem;
     color: ${(props) => props.theme.color.primary};
+    justify-content: space-around;
 
     p {
       color: rgba(${(props) => props.theme.color.tertiary});
@@ -31,17 +42,88 @@ export const AboutSection = styled.section`
     padding: 5rem 2rem 2rem 0;
   }
 
+  .tech_container {
+    ul {
+      width: 100%;
+      display: flex;
+      gap: 1rem;
+      flex-wrap: wrap;
+      overflow-wrap: break-word;
+
+      .tech-carousel {
+        display: flex;
+        gap: 1rem;
+        flex-wrap: wrap;
+        width: 100%;
+
+        li {
+          ${centralize}
+          background-color: ${(props) => props.theme.background.secondary};
+          padding: 1rem 2rem;
+          color: ${(props) => props.theme.color.primary};
+          border-radius: 1rem;
+          font-size: 1rem;
+          border: 1px solid ${(props) => props.theme.background.primary};
+          font-weight: 700;
+          box-shadow:
+            6px 6px 12px ${(props) => props.theme.shadow.secondary},
+            -6px -6px 12px ${(props) => props.theme.shadow.tertiary};
+          transition: all 0.4s ease;
+
+          &:hover {
+            filter: contrast(1.1);
+          }
+        }
+      }
+    }
+  }
+
+  /* Apenas em mobile e tablet o carrossel entra */
   @media ${({ theme }) => theme.device.mobile},
     ${({ theme }) => theme.device.tablet} {
     grid-template-columns: 1fr;
     height: 100dvh;
-    padding: 0;
+    padding: 0 5%;
+    align-items: center;
+    overflow: hidden;
+
+    .about_container {
+      padding: 0;
+      gap: 1rem;
+    }
+
+    .tech_container {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      gap: 1rem;
+      height: 10rem;
+      overflow: hidden;
+
+      .tech-carousel {
+        display: flex;
+        flex-wrap: nowrap;
+        width: max-content;
+        animation: ${scrollAnimation} 15s linear infinite;
+      }
+
+      ul {
+        display: flex;
+        gap: 2rem;
+        background-color: transparent;
+        opacity: 0.5;
+        height: 4rem;
+
+        li {
+          gap: 0.5rem;
+        }
+      }
+    }
 
     .card_container {
       display: flex;
       overflow-x: auto;
-      padding: 1rem 0;
-      -webkit-overflow-scrolling: touch;
+      padding: 0;
 
       ${CardBox} {
         flex-shrink: 0;
@@ -50,13 +132,12 @@ export const AboutSection = styled.section`
       }
     }
 
-    .card_description {
+    .description_container {
       order: -1;
-      padding: 0 1rem;
+      padding: 3rem 0 0 0;
       text-align: center;
       max-width: 25rem;
       width: 100%;
-      margin: 0 auto;
     }
   }
 `
